fbc94fe858313144703b7bda344b6693953f5c36
cchet <herzog.thomas81@gmail.com>
2022-10-08 13:47:34 +0200

    - All services use https now - Modified script for generating certs and truststore - Configured all services for https and secured rest-clients

--- generate-service-certificates.sh
+++ generate-service-certificates.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+SECURITY="security"
+PASSWORD="changeit"
+rm -rf ${SECURITY}
+mkdir ${SECURITY}
+
 function generate {
     echo "generating keyPair for service ${1}"
-    openssl genrsa -out ${1}/${1}RSAPrivateKey.pem 2048
-    openssl rsa -pubout -in ${1}/${1}RSAPrivateKey.pem -out ${1}/${1}PublicKey.pem
-    openssl pkcs8 -topk8 -nocrypt -inform pem -in ./${1}/${1}RSAPrivateKey.pem -outform pem -out ./${1}/${1}PKCS8PrivateKey.pem
+    openssl req -passout pass:${PASSWORD} -days 365 -subj /CN=${1}/  -newkey rsa:2048 -nodes -keyout ${SECURITY}/${1}.key -out ${SECURITY}/${1}.csr
+    openssl x509 -signkey ${SECURITY}/${1}.key -in ${SECURITY}/${1}.csr -req -days 365 -out ${SECURITY}/${1}.crt
+    openssl pkcs12 -password pass:${PASSWORD} -inkey ${SECURITY}/${1}.key -in ${SECURITY}/${1}.crt -export -out ${1}/${1}.p12
+    keytool -noprompt -import -storetype PKCS12 -storepass ${PASSWORD} -file ${SECURITY}/${1}.crt -alias ${1} -keystore ${SECURITY}/truststore.p12
 }
 
 generate store
-#generate order
-#generatecd warehouse
+generate order
+generate warehouse
+
+cp -f ${SECURITY}/truststore.p12 store/
+cp -f ${SECURITY}/truststore.p12 order/
+cp -f ${SECURITY}/truststore.p12 warehouse/
